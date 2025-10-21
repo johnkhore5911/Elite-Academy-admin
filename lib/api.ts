6996faceb9ac74750b8795ed31bcef7be4a8fe6a
johnkhore5911 <johnkhore26@gmail.com>
2025-10-21 15:36:46 +0530

    url changed

--- lib/api.ts
+++ lib/api.ts
@@ -4,9 +4,9 @@ import axios from "axios"
 //https://elite-academy-production.up.railway.app/
 // Create a base API instance
 const api = axios.create({
-  // baseURL: "https://elite-academy-ebon.vercel.app/api",
+  baseURL: "https://elite-academy-ebon.vercel.app/api",
   // baseURL: "https://elite-academy-production.up.railway.app/api",
-  baseURL: "http://192.168.18.15:4000/api",
+  // baseURL: "http://192.168.18.15:4000/api",
 })
 
 // Add a request interceptor to include the auth token
